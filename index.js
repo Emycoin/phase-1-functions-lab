e9dc65c770f2fe4d4959a2f1be07b46d74a04c62
Nish <nish@Nishs-Air.lan>
2021-06-29 14:48:51 -0400

    save changes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+function  distanceFromHqInBlocks(pickupLocation){
+    let distanceInBlocks = 42 - pickupLocation;
+     return Math.abs(distanceInBlocks);
+
+}
+function distanceTravelledInFeet(beginning, destination){
+    
+}
+
